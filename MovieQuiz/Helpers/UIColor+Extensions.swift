557836a484c8bf6e20bc1e26a952921fab3079ca
ccodderr <crptcharm@gmail.com>
2024-09-16 17:29:48 +0300

    Implemented MovieQuiz  application

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var greenСustom: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var redСustom: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var blackСustom: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var backgroundColor: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var grayСustom: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+}
